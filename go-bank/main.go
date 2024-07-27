08a67abfa1f00f00ec8b096cd8f726c004a4f9cc
Jhessica Kanda <jhessica.kanda@skillshare.com>
2024-07-27 20:45:42 -0300

    feat: add NewPostgresStorage to API Server call

--- go-bank/main.go
+++ go-bank/main.go
@@ -1,6 +1,18 @@
 package main
 
+import (
+	"log"
+)
+
 func main() {
-	server := NewAPIServer(":3000")
+	storage, err := NewPostgresStorage()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// fmt.Printf("%+v\n", storage)
+
+	server := NewAPIServer(":3000", storage)
 	server.Run()
 }
