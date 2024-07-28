d98e14cae0fbaf338735e028452e46dfdd5b4ab2
Jhessica Kanda <jhessica.kanda@skillshare.com>
2024-07-27 21:22:30 -0300

    feat: implementation of database functions
    - create database from scratch
    - insert a new account
    - get all accounts created

--- go-bank/storage.go
+++ go-bank/storage.go
@@ -11,6 +11,7 @@ type Storage interface {
 	CreateAccount(*Account) error
 	DeleteAccount(int) error
 	UpdateAccount(*Account) error
+	GetAccounts() ([]*Account, error)
 	GetAccountByID(int) (*Account, error)
 }
 
@@ -35,8 +36,67 @@ func NewPostgresStorage() (*PostgresStorage, error) {
 	return &PostgresStorage{db: db}, nil
 }
 
-func (s *PostgresStorage) CreateAccount(a *Account) error {
+func (s *PostgresStorage) Init() error {
+	return s.createAccountTable()	
+}
+
+func (s *PostgresStorage) createAccountTable() error {
+	query := `CREATE TABLE IF NOT EXISTS accounts (
+		id SERIAL PRIMARY KEY, 
+		first_name VARCHAR(50), 
+		last_name VARCHAR(50), 
+		number INTEGER, 
+		balance INTEGER, 
+		created_at TIMESTAMP
+
+	)`
+	_, err := s.db.Exec(query)
+	return err
+}
+
+func (s *PostgresStorage) CreateAccount(acc *Account) error {
+	query := `INSERT INTO accounts (
+		first_name, 
+		last_name, 
+		number, 
+		balance, 
+		created_at
+	) VALUES ($1, $2, $3, $4, $5)`
+	 
+	_, err := s.db.Query(query,
+		acc.FirstName, 
+		acc.LastName, 
+		acc.Number, 
+		acc.Balance, 
+		acc.CreatedAt)
+
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func (s *PostgresStorage) GetAccounts() ([]*Account, error) {
+	rows, err := s.db.Query("SELECT * FROM accounts")
+	
+	if err != nil {
+		return nil, err
+	}
+
+	accounts := []*Account{}
+
+	for rows.Next() {
+		acc := new(Account)
+		err = rows.Scan(&acc.ID, &acc.FirstName, &acc.LastName, &acc.Number, &acc.Balance, &acc.CreatedAt)
+		if err != nil {
+			return nil, err
+		}
+		
+		accounts = append(accounts, acc)
+	}
+
+	return accounts, nil
 }
 
 func (s *PostgresStorage) UpdateAccount(a *Account) error {
